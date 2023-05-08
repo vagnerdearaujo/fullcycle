e288ed90ea9fa2882004839022aaf7822cd93c03
Vagner de Araujo <vagnerdearaujo@gmail.com>
2023-05-08 17:00:40 -0300

    Desafio 1 - Docker. Criar uma imagem Docker com tamanho inferior à 2Mb. Criar um programa em Go que imprima a mensagem Full Cycle Rock !!! Ao executar a image o programa em Go deve ser executado para imprimir a imagem  Changes to be committed: 	new file:   desafios/docker/desafio1/Dockerfile 	new file:   desafios/docker/desafio1/go/Desafio1 	new file:   desafios/docker/desafio1/go/go.mod 	new file:   desafios/docker/desafio1/go/main.go

---
+++ desafios/docker/desafio1/go/main.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+	//O pacote fmt não foi utilizado por simples economia de espaço e de não haver a real necessidade de usá-lo neste desafio.
+	println("Full Cycle Rock!!!")
+	println("\n\n\nBy Vagner de Araujo - 2023-05")
+}
