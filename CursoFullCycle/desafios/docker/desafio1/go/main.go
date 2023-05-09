f6827c28321e94f6a9bde1652172d2a4ef11bde1
Vagner de Araujo <vagnerdearaujo@gmail.com>
2023-05-08 21:04:36 -0300

    Adição de saltos de linhas

--- CursoFullCycle/desafios/docker/desafio1/go/main.go
+++ CursoFullCycle/desafios/docker/desafio1/go/main.go
@@ -2,6 +2,6 @@ package main
 
 func main() {
 	//O pacote fmt não foi utilizado por simples economia de espaço e de não haver a real necessidade de usá-lo neste desafio.
-	println("Full Cycle Rock!!!")
-	println("\n\n\nBy Vagner de Araujo - 2023-05")
+	println("\n\n\n\nFull Cycle Rocks!!!")
+	println("\n\n\nBy Vagner de Araujo - 2023-05\n\n\n\n")
 }
