cce141beaf0febcfcad2a94974298bde0dc7f606
Vagner de Araujo <vagnerdearaujo@gmail.com>
2023-06-27 15:14:31 -0300

    Ajustes de pastas

--- desafios/docker/desafio1/go/go.mod
+++
@@ -1,3 +0,0 @@
-module Desafio1
-
-go 1.20
--- desafios/docker/desafio1/go/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	//O pacote fmt não foi utilizado por simples economia de espaço e de não haver a real necessidade de usá-lo neste desafio.
-	println("\n\n\n\nFull Cycle Rocks!!!")
-	println("\n\n\nBy Vagner de Araujo - 2023-05\n\n\n\n")
-}
